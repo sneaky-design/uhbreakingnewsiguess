bc270775216129980b6f840cc1b6d0eb8608b0a3
sneaky-design <85828142+sneaky-design@users.noreply.github.com>
2023-07-03 10:03:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function mystery(){
+    random = Math.floor(Math.random() * 11);
+    if (random == 1 ) {
+        window.location.href = 'https://cursoreffects.com/';
+    }
+    else if (random == 2 ) {
+        window.location.href = 'https://sliding.toys/mystic-square/8-puzzle/';
+    }
+    else if (random == 3 ) {
+        window.location.href = 'https://longdogechallenge.com/';
+}
+    else if (random == 4 ) {
+        window.location.href = 'https://maze.toys/mazes/medium/daily/';
+}
+    else if (random == 5 ) {
+        window.location.href = 'https://checkbox.toys/scale/';
+}
+    else if (random == 6 ) {
+        window.location.href = 'https://mondrianandme.com/';
+}
+    else if (random == 7 ) {
+        window.location.href = 'https://onesquareminesweeper.com/';
+}
+    else if (random == 8 ) {
+        window.location.href = 'https://jacksonpollock.org/';
+}
+    else if (random == 9 ) {
+        window.location.href = 'https://floatingqrcode.com/';
+}
+    else if (random == 10 ) {
+        window.location.href = 'https://puginarug.com/';
+}
+    else if (random == 0 ) {
+        window.location.href = 'http://burymewithmymoney.com/';
+}
+
+}
